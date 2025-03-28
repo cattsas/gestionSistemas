779f9ad994cd318c291c614aeb2d972ba2e04445
cattsas <cattsas1980@gmail.com>
2025-03-27 23:35:49 -0300

    se agrega redireccion al submit del update del articulo

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,25 +1,13 @@
 import NavLinks from "@/components/dashboard/NavLinks";
 export default function RootLayout({ children }) {
   return (
-   
-      
-       
-        <section className='flex flex-row w-full h-screen bg-gradient-to-r from-green-800 to-slate-800	'>
-          <div className='w-1/6 h-80vh mt-40 mb-0'>
-            <article >
-                <NavLinks/>
-            </article>
-          </div>
-          <section className='w-screen'>
-           {children}
-          </section>
-        </section>
-   
-      
-        
-        
-       
-       
-  
+    <section className="flex flex-row w-full h-screen bg-gradient-to-r from-green-800 to-slate-700	">
+      <div className="w-1/6 h-80vh mt-40 mb-0">
+        <article>
+          <NavLinks />
+        </article>
+      </div>
+      <section className="w-screen">{children}</section>
+    </section>
   );
 }
